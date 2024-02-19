c1e06eca62e30c5f04fc5ce5c0ce12f0a73e8876
Elm <elmliu6@gmail.com>
2024-02-19 20:28:55 +0800

    Added `ImageNet64`. Added dataloader support.

---
+++ config.py
@@ -0,0 +1,5 @@
+# Dataset
+imagenet_root = '/data/ljc/datasets/imagenet64/processed/'
+
+# Training
+bs_imagenet = 64
--- datahelpers.py
+++ datahelpers.py
@@ -1,32 +1,43 @@
 import torch
 import torchvision.transforms as transforms
 from torch.utils.data import Dataset, DataLoader
-import zipfile
 import os
+import config
 import numpy as np
 
 class ImageNet64(Dataset):
     def __init__(self, data_dir, transform=None):
         self.data_dir = data_dir
         self.transform = transform
-        self.image_files = os.listdir(data_dir)
+        self.image_files = self.get_img_list()
+        self.labels = [int(x.strip()) for x in open(data_dir + '/labels.txt', 'r').readlines() if x.strip()]
+
+    def get_img_list(self):
+        li = [x for x in os.listdir(self.data_dir) if x != 'labels.txt']
+        li = sorted(li, key=lambda x:int(x.split('.')[0]))      # Use file number to sort file list.
+        return li
 
     def __len__(self):
         return len(self.image_files)
 
     def __getitem__(self, idx):
         image_file = os.path.join(self.data_dir, self.image_files[idx])
-        with open(image_file, 'rb') as f:
-            image = np.fromfile(f, dtype=np.uint8)
-        image = image.reshape(64, 64, 3)  # 假设图像是 RGB 格式
+        image = np.load(image_file) # Load image. Shape: (64*64*3). Already normalized.
+        
         if self.transform:
             image = self.transform(image)
         return image
     
-# 定义数据转换
-transform = transforms.Compose([
-    transforms.ToTensor(),  # 转换为张量
-    transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])  # 标准化
-])
+# transform = transforms.Compose([
+#     transforms.ToTensor(), 
+#     transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+# ])
 
-test_dataset = ImageNet64(data_dir=r'E:\datasets\Imagenet64_val', transform=transform)
+def get_imagenet_loaders():
+    train_dataset = ImageNet64(data_dir=config.imagenet_root + '/train')
+    val_dataset = ImageNet64(data_dir=config.imagenet_root + '/val')
+    
+    train_loader = DataLoader(train_dataset, batch_size=config.bs_imagenet, shuffle=True)
+    val_loader = DataLoader(val_dataset, batch_size=config.bs_imagenet, shuffle=False)
+    
+    return train_loader, val_loader
